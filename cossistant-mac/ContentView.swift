c51b2faa6c6ba0a8831fb2ea5ac38700a240a848
Dominik Seemayr <seemayr@me.com>
2026-04-09 13:06:02 +0700

    Initial Commit

---
+++ cossistant-mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cossistant-mac
+//
+//  Created by Dominik Seemayr on 09.04.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cossistant-mac/cossistant_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  cossistant_macApp.swift
+//  cossistant-mac
+//
+//  Created by Dominik Seemayr on 09.04.26.
+//
+
+import SwiftUI
+
+@main
+struct cossistant_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
